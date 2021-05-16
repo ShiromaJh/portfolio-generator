66727c5d017e35a6dec854496f3908c457580fbb
Justin Hoffman <justinhoffman193@gmail.com>
2021-05-16 15:40:46 -0500

    Corrected code syntax error

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    //this...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const printProfileData = (profileDataArr) => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
+  console.log("================");
 
-console.log('================');
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
 
-//is the same as this...
-profileDataArr.forEach(profileItem => console.log(profileItem));
-
-
-printProfileData(profileDataArr)}
+printProfileData(profileDataArgs);
